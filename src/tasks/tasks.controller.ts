d72b10149c601fdf55a395776e63862b1bfd4699
Laurenz Saam <laurenz.saam@gmail.com>
2023-07-19 11:07:13 +0200

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
+
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskDto } from './dto/update-task-status.dto';
+import { Task } from './task.entity';
+
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  private logger = new Logger('TasksController');
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getTasks(
+    @Query() filterDto: GetTaskFilterDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filter ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
+    return this.taskService.getTasks(filterDto, user);
+  }
+  @Get(':id')
+  getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    return this.taskService.getTaskById(id, user);
+  }
+  @Post()
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.taskService.createTask(createTaskDto, user);
+  }
+  @Delete(':id')
+  deleteTaskById(
+    @Param('id') id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.taskService.deleteTaskById(id, user);
+  }
+  @Patch(':id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body() updateTastDto: UpdateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.taskService.updateTask(id, updateTastDto.status, user);
+  }
+}
